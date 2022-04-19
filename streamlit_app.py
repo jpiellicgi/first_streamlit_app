72f90567e384a42183903dd0d7db8e75426bc974
jpiellicgi <92184385+jpiellicgi@users.noreply.github.com>
2022-04-19 16:34:15 -0500

    Update streamlit_app.py
    added default fruit selection
    changes table to fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#let users pick their fruits f/ a multislect
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#let users pick their fruits f/ a multislect; predefine default selection
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_lis.loc[fruits_selected]
 
 #display the table of fruits
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
