a8a3b313a716cfc447c566168d57c7c31342944e
jpiellicgi <92184385+jpiellicgi@users.noreply.github.com>
2022-04-21 16:35:29 -0500

    Update streamlit_app.py
    test comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #let users pick their fruits f/ a multislect; predefine default selection
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
