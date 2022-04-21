246882cd2be92e6e43db779ce71032af83cb9bb6
jpiellicgi <92184385+jpiellicgi@users.noreply.github.com>
2022-04-21 16:34:47 -0500

    Update streamlit_app.py
    added normalized fruityvice table

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,15 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #let users pick their fruits f/ a multislect; predefine default selection
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-fruits_to_show = my_fruit_lis.loc[fruits_selected]
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+#fruits_to_show = my_fruit_lis.loc[fruits_selected]
 
 #display the table of fruits
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruitvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
